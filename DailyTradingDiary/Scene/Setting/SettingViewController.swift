3e72ba90283151a4a13040a8a94b52abcc5f62f3
minhye9731 <jenney62639731@gmail.com>
2022-09-18 04:07:05 +0900

    설정화면 생성 및 화면이동 연결

---
+++ DailyTradingDiary/Scene/Setting/SettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingViewController.swift
+//  DailyTradingDiary
+//
+//  Created by 강민혜 on 9/17/22.
+//
+
+import UIKit
+
+class SettingViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+        self.tabBarController?.tabBar.isHidden = true
+        
+    }
+    
+
+    
+}
