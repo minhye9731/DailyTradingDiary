19b77ee6186bb8f72d97b20b9e89b20892c2875f
minhye9731 <jenney62639731@gmail.com>
2022-11-01 04:17:17 +0900

    문의하기, 리뷰쓰기, 오픈소스 라이선스, 버전표기 기능 추가 완료

--- DailyTradingDiary/Scene/Setting/SettingViewController.swift
+++ DailyTradingDiary/Scene/Setting/SettingViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import MessageUI
+import AcknowList
+import Toast
 
 struct SettingList: Hashable {
     let id = UUID().uuidString
@@ -15,7 +17,7 @@ struct SettingList: Hashable {
 }
 
 
-class SettingViewController: BaseViewController {
+class SettingViewController: BaseViewController, MFMailComposeViewControllerDelegate {
     
     let mainView = SettingView()
     
@@ -23,7 +25,7 @@ class SettingViewController: BaseViewController {
         SettingList(title: "문의하기", subTitle: ""),
         SettingList(title: "리뷰쓰기", subTitle: ""),
         SettingList(title: "오픈소스 라이선스", subTitle: ""),
-        SettingList(title: "버전", subTitle: "x.x.x")
+        SettingList(title: "버전", subTitle: "0.0.0")
     ]
     
     private var dataSource: UICollectionViewDiffableDataSource<Int, SettingList>!
@@ -36,51 +38,12 @@ class SettingViewController: BaseViewController {
     
     override func configure() {
         self.tabBarController?.tabBar.isHidden = true
+        mainView.collectionView.delegate = self
         setNav()
     }
     
 }
 
-// MARK: - 기타함수
-extension SettingViewController {
-    
-    func setNav() {
-        self.navigationItem.title = "설정"
-        self.navigationController?.navigationBar.tintColor = .pointColor
-        let navibarAppearance = UINavigationBarAppearance()
-        navibarAppearance.backgroundColor = .backgroundColor
-        navibarAppearance.titleTextAttributes = [.foregroundColor: UIColor.mainTextColor, .font: UIFont.systemFont(ofSize: 18, weight: .bold)]
-        
-        self.navigationItem.scrollEdgeAppearance = navibarAppearance
-        self.navigationItem.standardAppearance = navibarAppearance
-    }
-    
-    func sendEmailTapped() {
-        
-        if MFMailComposeViewController.canSendMail() {
-            
-        }
-        else {
-            
-        }
-        
-    }
-    
-    func writeReviewTapped() {
-        
-        if let appstoreUrl = URL(string: "https://apps.apple.com/app/id{앱스토어ID}") {
-            var urlComp = URLComponents(url: appstoreUrl, resolvingAgainstBaseURL: false)
-            urlComp?.queryItems = [
-                URLQueryItem(name: "action", value: "write-review")
-            ]
-            guard let reviewUrl = urlComp?.url else {
-                return
-            }
-            UIApplication.shared.open(reviewUrl, options: [:], completionHandler: nil)
-        }
-        
-    }
-}
 
 // MARK: - compositional
 extension SettingViewController {
@@ -99,6 +62,10 @@ extension SettingViewController {
             content.text = itemIdentifier.title
             content.secondaryText = itemIdentifier.subTitle
             content.textProperties.color = .mainTextColor
+            
+            // 버전정보 표기
+            if indexPath.row == 3 { content.secondaryText = self.currentAppVersion() }
+            
             cell.contentConfiguration = content
             
             var background = UIBackgroundConfiguration.listPlainCell()
@@ -119,4 +86,105 @@ extension SettingViewController {
         dataSource.apply(snapshot)
     }
     
+}
+
+extension SettingViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        switch indexPath.row {
+        case 0: self.sendEmailTapped()
+        case 1: self.writeReviewTapped()
+        case 2: self.showPackageList()
+        case 3: print("버전확인 - \(self.currentAppVersion())")
+        default: print("tap")
+        }
+    }
+}
+
+
+// MARK: - 기타함수
+extension SettingViewController {
+    
+    func setNav() {
+        self.navigationItem.title = "설정"
+        self.navigationController?.navigationBar.tintColor = .pointColor
+        let navibarAppearance = UINavigationBarAppearance()
+        navibarAppearance.backgroundColor = .backgroundColor
+        navibarAppearance.titleTextAttributes = [.foregroundColor: UIColor.mainTextColor, .font: UIFont.systemFont(ofSize: 18, weight: .bold)]
+        
+        self.navigationItem.scrollEdgeAppearance = navibarAppearance
+        self.navigationItem.standardAppearance = navibarAppearance
+    }
+    
+    // 메일 보내기
+    func sendEmailTapped() {
+        
+        if MFMailComposeViewController.canSendMail() {
+            
+            let compseVC = MFMailComposeViewController()
+            compseVC.mailComposeDelegate = self
+            
+            compseVC.setToRecipients(["jenney62639731@gmail.com"])
+            compseVC.setSubject("Trady 문의사항")
+            compseVC.setMessageBody("문의 사항을 상세히 입력해주세요.", isHTML: false)
+            compseVC.modalPresentationStyle = .overFullScreen
+            
+            self.present(compseVC, animated: true, completion: nil)
+            
+        }
+        else {
+            self.showSendMailErrorAlert()
+        }
+        
+    }
+    
+    // 디바이스내 mail앱을 이용할 수 없는 경우
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "아이폰 이메일 설정을 확인 후 재시도해주세요.", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "확인", style: .default)
+        
+        sendMailErrorAlert.addAction(confirm)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    // 메일 보내기 완료 후, 창닫기
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        
+        
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // 리뷰쓰기
+    func writeReviewTapped() {
+        
+        if let appstoreUrl = URL(string: "https://apps.apple.com/app/id6443574203") {
+            var urlComp = URLComponents(url: appstoreUrl, resolvingAgainstBaseURL: false)
+            urlComp?.queryItems = [
+                URLQueryItem(name: "action", value: "write-review")
+            ]
+            guard let reviewUrl = urlComp?.url else {
+                return
+            }
+            UIApplication.shared.open(reviewUrl, options: [:], completionHandler: nil)
+        }
+    }
+    
+    // 라이브러리 리스트 화면
+    func showPackageList() {
+        let vc = AcknowListViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // 현재 버전
+    func currentAppVersion() -> String {
+        if let info: [String: Any] = Bundle.main.infoDictionary,
+           let currentVersion: String = info["CFBundleShortVersionString"] as? String {
+            return currentVersion
+        }
+        return "nil"
+    }
 }
